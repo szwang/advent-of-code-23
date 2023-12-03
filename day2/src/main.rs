fa4b11152f907f0b226291bf43b026fa53d1ba79
Suzanne Wang <suzanneswang@gmail.com>
2023-12-02 23:05:04 -0800

    part2! day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,54 +1,68 @@
-use std::{fs, collections::HashMap};
+use std::fs;
 use regex::Regex;
-
-// 12 red cubes, 13 green cubes, and 14 blue cubes
 
 fn main() {
     let file_content = fs::read_to_string("input.txt")
         .expect("should read file successfully");
 
-    // Split lines
-    // Get line after colon
-    let color_vec: Vec<Vec<(i32, i32, i32)>> = file_content.lines().map(|line| {
-        // ["2 red, 2 green", "6 red, 3 green", "2 red, 1 green, 2 blue", "1 red"]
-        get_colors_string_vec(line).iter().map(|set| get_color_tuple(set)).collect()
-    }).collect();
+    // Create a vec of colors by game [[(r,g,b)]]
+    let color_vec: Vec<Vec<(i32, i32, i32)>> = 
+        file_content.lines().map(|line| create_color_vec(line)).collect();
 
-    println!("{:?}", color_vec);
+    //// Part 1
+    // let result = part_1(color_vec);
 
-    let result = color_vec.iter().enumerate().fold(0, |acc, (i, game_set)| {
-        let game_number = i + 1;
-        // Check to see if r <= 12, g <= 13, b <= 14
+    //// Part 2
+    let result = part_2(color_vec);
+
+    println!("{result}");
+}
+
+fn part_1(color_vec: Vec<Vec<(i32, i32, i32)>>) -> i32 {
+    color_vec.iter().enumerate().fold(0, |acc, (i, game_set)| {
+        let game_number: i32 = (i + 1).try_into().unwrap();
         let all_match = game_set.iter().all(|set| set_is_possible(set));
         if all_match {
             return acc + game_number;
-        } else {
-            return acc;
         }
-    });
+        return acc;
+    })
+}
 
-    println!("{result}");
+fn part_2(color_vec: Vec<Vec<(i32, i32, i32)>>) -> i32 {
+    color_vec.iter().fold(0, |acc, game_set| {
+        // retrieve largest value of each tuple place in game set
+        let largest = game_set.iter().fold((0,0,0), |(acc_r, acc_g, acc_b), (set_r, set_g, set_b)| {
+            let r = if set_r > &acc_r { set_r } else { &acc_r };
+            let g = if set_g > &acc_g { set_g } else { &acc_g };
+            let b = if set_b > &acc_b { set_b } else { &acc_b };
+
+            (*r, *g, *b)
+        });
+        acc + (largest.0 * largest.1 * largest.2)
+    })
 }
 
 fn set_is_possible((r,g,b): &(i32, i32, i32)) -> bool {
     *r <= 12 && *g <= 13 && *b <= 14
 }
 
-fn get_colors_string_vec(line: &str) -> Vec<String> {
+fn create_color_vec(line: &str) -> Vec<(i32, i32, i32)> {
     let color_regex = Regex::new(r":\s(.*)").unwrap();
     let regex_result = color_regex.captures(line).unwrap();
     let colors_string = regex_result.get(1).unwrap().as_str();
-    colors_string.split("; ").map(str::to_string).collect()
+    let string_vec: Vec<String> = colors_string.split("; ").map(str::to_string).collect();
+    string_vec.iter().map(|set| get_color_tuple(set)).collect()
 }   
 
 fn get_color_tuple(string: &str) -> (i32, i32, i32) {
-    let r = retrieve_number_before("red", string);
-    let g = retrieve_number_before("green", string);
-    let b = retrieve_number_before("blue", string);
+    let r = get_color_num("red", string);
+    let g = get_color_num("green", string);
+    let b = get_color_num("blue", string);
     (r, g, b)
 }
 
-fn retrieve_number_before(color: &str, text: &str) -> i32 {
+fn get_color_num(color: &str, text: &str) -> i32 {
     let regex_pattern = format!(r"(\d+)\s*{}", color);
     let re = Regex::new(&regex_pattern).unwrap();
     match re.captures(text) {
